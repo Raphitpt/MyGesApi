40bd80018b5be42f2ddf7c298ecbaedd0977d937
Raphaël Tiphonet <raphael.tiphonet@mink-agency.com>
2025-09-27 21:19:20 +0200

    feat: change to tsup because tsdown not work in expo

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from "tsdown";
+import { defineConfig } from "tsup";
 
 export default defineConfig({
   clean: true,
   dts: true,
   entry: ["src/index.ts"],
   format: ["cjs", "esm"],
-  minify: true,
+  minify: "terser",
   outDir: "dist",
   sourcemap: true,
+  splitting: false,
   treeshake: true
 });
